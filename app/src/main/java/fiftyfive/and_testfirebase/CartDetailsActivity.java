8b53179eff5b4cdbd637416f3ad0bd1eed185a5d
fhkhoury <f.khouryat@gmail.com>
2016-08-16 14:02:26 +0200

    Firebase is OK

---
+++ app/src/main/java/fiftyfive/and_testfirebase/CartDetailsActivity.java
@@ -0,0 +1,56 @@
+package fiftyfive.and_testfirebase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartDetailsActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    Cart cart = new Cart();
+    ListView mListView;
+    ArrayList panier = new ArrayList<Item>();
+    Bundle zeBundle = new Bundle();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_details);
+
+        final Intent zeIntent = getIntent();
+        zeBundle = zeIntent.getBundleExtra(MainActivity.SUPERBUNDLE);
+        panier = zeIntent.getParcelableArrayListExtra("hello");
+        Log.d("ACTION: ", "Bundle récupéré");
+        Log.d("INFO: ", zeBundle.getString("screenName"));
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // send a hit to GA to log the screen name
+        zeBundle.putString("screenName", "Cart details");
+        mFirebaseAnalytics.logEvent("openScreen", zeBundle);
+        Log.d("TAG: ", "openScreen - Cart details sent.");
+        Log.d("INFO: ", zeBundle.getString("screenName"));
+
+        //Récupérer  la listView
+        mListView = (ListView) findViewById(R.id.listView);
+        //remplir la liste avec le panier
+        for (int i=0; i<cart.getNumberOfArticles(); i++){
+            panier.add(cart.getItem(i).name);
+        }
+        //afficjer la liste
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CartDetailsActivity.this, android.R.layout.simple_list_item_1, panier);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(CartDetailsActivity.this,simple_list_item_1, panier.toString());
+        mListView.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/fiftyfive/and_testfirebase/MainActivity.java
+++ app/src/main/java/fiftyfive/and_testfirebase/MainActivity.java
@@ -1,16 +1,29 @@
 package fiftyfive.and_testfirebase;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.crash.FirebaseCrash;
 import com.google.firebase.messaging.FirebaseMessagingService;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 import static com.google.firebase.crash.FirebaseCrash.*;
 import static fiftyfive.and_testfirebase.MyFirebaseMessagingService.*;
@@ -18,32 +31,73 @@ import static fiftyfive.and_testfirebase.MyFirebaseMessagingService.*;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    Spinner catalogue;
+    Cart cart = new Cart();
+    public final static String SUPERBUNDLE = "DataLayer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-
-        // send a hit when the app is opened
+        // send a hard-coded hit to FB when the app is opened
         Bundle bundle = new Bundle();
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
-        // envoyer un tag d'écran via GTM vers Firebase & GA
-        bundle.clear();
-        bundle.putString("screenName","Home");
-        mFirebaseAnalytics.logEvent("openScreen", bundle);
+        Log.d("TAG: ", "APP_OPEN sent.");
 
+        // send a hit to GA to log the screen name
+        final Bundle bundle1 = new Bundle();
+        bundle1.putString(FirebaseAnalytics.Param.VALUE, "HomePage");
+        bundle1.putString("screenName", "HomePage");
+        mFirebaseAnalytics.logEvent("openScreen", bundle1);
+        Log.d("TAG: ", "openScreen - HomePage sent.");
+
+        //checkons ce qu'il se passe dans le data layer
+        Log.d("GUIGUI : ", bundle1.getString("screenName"));
+        bundle1.putString("screenName","Blabla");
+        Log.d("GUIGUI2 : ", bundle1.getString("screenName"));
+
+
+        //Récupération du Spinner déclaré dans le fichier main.xml de res/layout
+        catalogue = (Spinner) findViewById(R.id.spinner);
+        //Création d'une liste d'élément à mettre dans le Spinner(pour l'exemple)
+        List exempleList = new ArrayList();
+        exempleList.add("Playstation - 400€");
+        exempleList.add("X-Box - 300€");
+        exempleList.add("PSP - 100€");
+
+		/*Le Spinner a besoin d'un adapter pour sa presentation alors on lui passe le context(this) et
+                un fichier de presentation par défaut( android.R.layout.simple_spinner_item)
+		Avec la liste des elements (exemple) */
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, exempleList );
+
+        /* On definit une présentation du spinner quand il est déroulé         (android.R.layout.simple_spinner_dropdown_item) */
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Enfin on passe l'adapter au Spinner et c'est tout
+        catalogue.setAdapter(adapter);
+
+        /*Button addToCart = (Button) findViewById(R.id.button_cliCkCart);
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewCart(bundle1, cart);
+            }
+        });*/
     }
 
-    //méthode pour faire partir un event après avoir cliqué sur un bouton
+    /*méthode pour faire partir un event après avoir cliqué sur un bouton
     //event codé en dur et envoyé sur Firebase
     public void click2Fire_hard(View v) {
         Bundle myBundle = new Bundle();
         mFirebaseAnalytics.logEvent("click2Fire_hard", myBundle);
-    }
+    }*/
+
+
 
     //méthode pour faire partir un event après avoir cliqué sur un bouton
     //event codé pour utilisation via GTM et envoyé sur Firebase & GA
@@ -53,18 +107,49 @@ public class MainActivity extends AppCompatActivity {
         thisBundle.putString("eventAction", "fire");
         thisBundle.putString("eventLabel", "click2Fire_GTM");
         mFirebaseAnalytics.logEvent("click2Fire_GTM", thisBundle);
-    }
+        Log.d("TAG: ", "Click2FIre_GTM sent.");
+        Toast.makeText(getApplicationContext(), "Click2FIre_GTM sent.", Toast.LENGTH_SHORT).show();
 
-    //Send a notification
-    public void sendNotification(View v) {
-        //MyFirebaseMessagingService.sendNotification("Mon MEssage");
     }
 
     // Make a crash
     public void crash(View v){
         report(new Exception("My first Android non-fatal error"));
-        FirebaseCrash.log("App is crashed");
+        FirebaseCrash.log("App has crashed");
+        Log.d("CRASH: ", "App has crashed, Buddy!");
+        Toast.makeText(getApplicationContext(), "App has crashed, Buddy!", Toast.LENGTH_SHORT).show();
+    }
 
+    //Add to Cart
+    public void addToCart(View v){
+        String selectedItemName = catalogue.getSelectedItem().toString();
+        Item item = new Item();
+        item.getItemDetails(selectedItemName);
+        //cart.addItem(item);
+        // send a hard-coded hit to FB when the app is opened
+        Bundle bundle = new Bundle();
+        //Remplir le bundle avec les variables pour eeng
+        bundle.putString(FirebaseAnalytics.Param.PRODUCT_ID, "123456");
+        bundle.putString(FirebaseAnalytics.Param.PRODUCT_NAME, selectedItemName);
+        bundle.putString(FirebaseAnalytics.Param.PRODUCT_CATEGORY, "Console");
+        bundle.putDouble(FirebaseAnalytics.Param.PRICE, 399.99);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_TO_CART, bundle);
+        //Log.d("INFO: ", item.sku);
+        //Log.d("INFO: ", item.name);
+        //Log.d("INFO: ", item.category);
+        //Log.d("INFO: ", item.price);
+        Log.d("TAG: ", "ADD_TO_CART sent.");
+        Log.d("ACTION: ", selectedItemName +" has been added to cart.");
+        Toast.makeText(getApplicationContext(), selectedItemName +" has been added to cart.", Toast.LENGTH_SHORT).show();
+    }
+
+    //View your cart
+    public void viewCart (Bundle bundle, Cart cart){
+        Intent intent = new Intent(MainActivity.this, CartDetailsActivity.class);
+        intent.putExtra(SUPERBUNDLE, bundle); // pour passer le data layer
+        //intent.putExtra("cart", (Parcelable) cart.listeProduits);//pour passer le cart à envoyer
+        startActivityForResult(intent, 0);
+        Log.d("INFO: ", bundle.getString("screenName"));
     }
 
     @Override
